31e3df7bf58c696e8b90627c0dc77fac9732b4e8
godcrampy <sahilbondre@gmail.com>
2019-05-08 09:40:52 +0530

    Added Sessions

--- lecture-2/application.py
+++ lecture-2/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_session import Session
 import datetime
 
@@ -8,6 +8,7 @@ notes = []
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
+
 
 @app.route('/')
 def index():
@@ -43,9 +44,11 @@ def form():
 
 @app.route('/notes', methods=['GET', 'POST'])
 def todo():
+    if session.get('notes') is None:
+        session['notes'] = []
     if request.method == 'POST':
         note = request.form.get('note')
-        notes.append(note)
+        session['notes'].append(note)
 
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=session['notes'])
     
